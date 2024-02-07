5887e4fe7d21627572b59cfd08a96914179d1c16
Chad Juliano <cjuliano@kinetica.com>
2024-02-07 12:14:35 -0600

    update version

--- src/langchain_kinetica/__init__.py
+++ src/langchain_kinetica/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2024, Chad Juliano, Kinetica DB Inc.
 ##
 
-__version__ = "1.0b1"
+__version__ = "1.0.0"
 
 from .llm_chat import KineticaChatLLM
 from .sql_output import KineticaSqlOutputParser, SqlResponse
